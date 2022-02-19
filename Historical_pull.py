7fad1feb4d752e3c628c3a5d8d275e8a9373dfb8
abhaysh <abhaysh@umich.edu>
2022-02-19 18:13:54 -0500

    Class setup

---
+++ Historical_pull.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+import numpy
+import matplotlib
+import seaborn as sb
+import json
+import csv
+
+
+class Data_Formatter:
+    def stock_name(input, start_year=0, end_year=0):
+        pass
+
+    def data_pull(input, start_year=0, end_year=0):
+        pass
+
+    def data_format(input, start_year=0, end_year=0):
+        pass
+
+
+
